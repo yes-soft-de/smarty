39b6995566af4a3bb647255aee7fb121a81779c6
Zolfekar Seleten <zolfekarseleten94@gmail.com>
2020-07-21 16:55:13 +0300

    add course_details_page design

---
+++ application/smart_y/lib/ui/screen/course_details_page/Course_details_page.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smarty/ui/widget/image_icon.dart';
+
+class CourseDetailPage extends StatefulWidget {
+  @override
+  _CourseDetailPageState createState() => _CourseDetailPageState();
+}
+
+class _CourseDetailPageState extends State<CourseDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff5E239D),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: ImageAsIcon(
+                img:'assets/goback.png',
+                height: 20.0,
+                width: 30.0,
+              ),
+              onPressed: () {
+
+              },
+
+            );
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+               Image(
+                 image: AssetImage('assets/course_image.png'),
+                 width: MediaQuery.of(context).size.width,
+               ),
+            Container(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 20),
+              child: Row(
+                
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('Weekly progress'),
+                  Text('For 50 \$'),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(width: MediaQuery.of(context).size.width*0.1,),
+                Text('Weekly progress on dieting',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 9,
+                  ),
+                ),
+              ],
+            ),
+
+            Container(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.favorite,color: Colors.grey,),
+                      Text('42 Likes',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10,
+                      ),)
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.comment,color: Colors.grey,),
+                      Text('7 Comments',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        ),)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsetsDirectional.fromSTEB(20, 20, 20, 20),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 10, 0),
+                    child: Image(
+
+                      image: AssetImage('assets/profilePic.png'
+                      ),
+                      height: MediaQuery.of(context).size.width*0.2,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Alex Smith'),
+                      Text('20 April at 4:20 PM',
+                      style: TextStyle(
+                      color: Colors.grey,
+                        fontSize: 9,
+                      )
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            Container (
+              padding: const EdgeInsets.all(16.0),
+              width: MediaQuery.of(context).size.width*0.85,
+              child: Text ("Long Text 2, Long Text 2, Long Text 2, Long Text 2, Long Text 2, Long Text 2, Long Text 2, Long Text 2, Long Text 2, Long Text 2, Long Text 2", textAlign: TextAlign.center),
+            ),
+
+            Divider(),
+            Container(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.comment,color: Colors.grey,),
+                      Text('7 Comments',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        ),)
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.favorite,color: Colors.grey,),
+                      Text('42 Likes',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        ),)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsetsDirectional.fromSTEB(20, 20, 20, 20),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 10, 0),
+                    child: Image(
+
+                      image: AssetImage('assets/profile_pic.png'),
+                      height: MediaQuery.of(context).size.width*0.2,
+                    ),
+                  ),
+
+                  Container(
+                    width: MediaQuery.of(context).size.width*0.6,
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.attach_file,color: Colors.grey,),
+                          hintStyle: TextStyle(fontSize: 10),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          filled: true,
+
+                        fillColor: Color(0xffebecfd),
+                        hintText: 'Write comment...'
+                      ),
+                    ),
+                  ),
+
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
