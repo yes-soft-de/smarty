175b8448450ca20d31b332f59fee8a611cd8a46c
Zolfekar Seleten <zolfekarseleten94@gmail.com>
2020-08-24 11:37:59 +0300

    add programs manager

---
+++ application/smart_y/lib/home/manager/programs/programs.manager.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:inject/inject.dart';
+import 'package:smarty/home/repository/programs_page/programs_page.repository.dart';
+import 'package:smarty/home/response/course_details_response/course_details_response.dart';
+
+@provide
+class ProgramsManager{
+  ProgramsRepository _programsRepository;
+
+  ProgramsManager(this._programsRepository);
+
+  Future<List<CourseDetailsResponse>> getPrograms() async{
+    return  await _programsRepository.getPrograms();
+  }
+
+}
