2ce409cb3a4b779e62ab82c8681c20137c91d056
Talal Danoun <tald7344@gmail.com>
2020-09-24 15:52:36 +0300

    delete theme files

--- site/theme/WPLMS/includes/chronjobs.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* ============ WPLMS Chron Jobs ============== */
-/* 1. Email Remider for Course expiration
-/* 2. Email reminder for Event expiration
-/* 3. Email reminder for Assignment timer expiration.
-/* ===============================================*/
--- site/theme/WPLMS/templates/course/bottomblank.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-do_action('wplms_single_course_content_end');
-
-?>
-</div></div>
-</section>
