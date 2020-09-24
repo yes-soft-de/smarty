a4e532cb768b44e3ae24cf5365f2db11dbbe7b76
Talal Danoun <tald7344@gmail.com>
2020-09-24 15:15:41 +0300

    delete all wplms theme

--- site/theme/wplms2/options/fields/button_set/field_button_set.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('.buttonset').buttonset({items: "button, input[type=radio],input[type=button], input[type=submit]"});
-});
--- site/theme/wplms2/templates/groups/bottom.php
+++
@@ -1,7 +0,0 @@
-					<?php do_action( 'bp_after_group_home_content' ); ?>
-					
-				</div>
-			</div><!-- .padder -->
-		</div><!-- #container -->
-	</div>
-</section>	
